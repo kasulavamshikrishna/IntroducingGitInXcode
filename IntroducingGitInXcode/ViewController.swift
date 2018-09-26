a5698ae4b3f53f5179685440a0eb30b2054c3cb3
Vamshi Krishna Kasula <vamshios@yahoo.com>
2018-09-26 17:14:11 +0530

    Added only print method with a message.

--- IntroducingGitInXcode/ViewController.swift
+++ IntroducingGitInXcode/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        print("Introducing github account in xcode..")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
